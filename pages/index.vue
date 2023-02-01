2334baf4e600e3ca7021b6824c2d9f9fabc1dd59
mulagetmula <124209629+mulagetmula@users.noreply.github.com>
2023-02-01 23:27:10 +0200

    Commit from TeleportHQ

---
+++ pages/index.vue
@@ -0,0 +1,268 @@
+<template>
+  <div class="home-container">
+    <img
+      alt="image"
+      src="/playground_assets/rrrrrrr-1500w.png"
+      class="home-image"
+    />
+    <span class="home-text">
+      OFICIĀLS MRGRECORDS X NARKASHMANIA X LVBROK34LYFE PROJEKTS
+    </span>
+    <img
+      alt="image"
+      src="/playground_assets/untitled-4-ewefweee-200w.png"
+      class="home-image1"
+    />
+    <span class="home-text01">
+      <span class="home-text02">(</span>
+      <span class="home-text03">
+        MĒS NEUZŅEMAMIES ATBILDĪBU PA JŪSU VESELĪBU)
+      </span>
+      <br class="home-text04" />
+      <br class="home-text05" />
+      <span class="home-text06">
+        Vai tu jūties kā nereālais lohs? Fanats? Narkaš? Vai dažreiz jautā: Kapēc
+        man?
+      </span>
+      <br class="home-text07" />
+      <br class="home-text08" />
+      <span class="home-text09">
+        Ja vēlies lai šis viss pazūd šis krekls salabos visas tavas phsihiskās,
+        fiziskās, mentālās problēmas.
+      </span>
+      <br class="home-text10" />
+      <br class="home-text11" />
+      <span class="home-text12">
+        Kā arī atbildēs uz visiem taviem jautājumiem:
+      </span>
+      <br class="home-text13" />
+      <span class="home-text14">Kapēc?</span>
+      <br class="home-text15" />
+      <span class="home-text16">Kapēc es?</span>
+      <br class="home-text17" />
+      <span class="home-text18">Kā dēl?</span>
+      <br class="home-text19" />
+      <span class="home-text20">Kapēc sēžam būdiņā?</span>
+      <br class="home-text21" />
+      <span class="home-text22">Velc ārā?</span>
+      <br class="home-text23" />
+      <span class="home-text24">utt.</span>
+      <br class="home-text25" />
+      <br class="home-text26" />
+      <span class="home-text27">
+        + izskatisies hella fresh velkot šito huijnu ārā.
+      </span>
+      <br class="home-text28" />
+      <br class="home-text29" />
+      <br class="home-text30" />
+      <br class="home-text31" />
+      <br class="home-text32" />
+      <span class="home-text33">
+        (šis produkts nepalīdzēs pilnīgi nekam, nu mošk izskatīsies fresh, bet es
+        huij viņ)
+      </span>
+    </span>
+    <span class="home-text34">NARKASHMANIA PROMO TEE</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  head: {
+    title: 'Roasted Digital Seahorse',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Roasted Digital Seahorse',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  background-color: #000000;
+}
+.home-image {
+  width: 100%;
+  object-fit: cover;
+}
+.home-text {
+  color: rgb(255, 0, 0);
+}
+.home-image1 {
+  top: 401px;
+  right: 29px;
+  width: 100px;
+  position: absolute;
+  object-fit: cover;
+}
+.home-text01 {
+  top: 410px;
+  left: 61px;
+  color: rgb(255, 255, 255);
+  width: var(--dl-size-size-xxlarge);
+  position: absolute;
+  font-size: 15px;
+  max-width: var(--dl-size-size-medium);
+  min-width: var(--dl-size-size-xxlarge);
+  align-self: flex-start;
+}
+.home-text02 {
+  font-size: 10px;
+}
+.home-text03 {
+  font-size: 10px;
+}
+.home-text04 {
+  font-size: 10px;
+}
+.home-text33 {
+  font-size: 7px;
+}
+.home-text34 {
+  top: 347px;
+  left: 0px;
+  color: rgb(253, 2, 2);
+  right: 0px;
+  margin: auto;
+  position: absolute;
+  font-size: 25px;
+  font-style: normal;
+  text-align: center;
+  font-family: Merriweather;
+  font-weight: 400;
+}
+@media(max-width: 479px) {
+  .home-text {
+    color: rgb(255, 0, 0);
+    font-size: 12px;
+  }
+  .home-image1 {
+    right: 20px;
+    bottom: 104px;
+  }
+  .home-text01 {
+    top: 593px;
+    left: 35px;
+    color: rgb(255, 255, 255);
+    font-size: 12px;
+  }
+  .home-text02 {
+    font-size: 8px;
+  }
+  .home-text03 {
+    font-size: 8px;
+  }
+  .home-text04 {
+    font-size: 8px;
+  }
+  .home-text05 {
+    font-size: 12px;
+  }
+  .home-text06 {
+    font-size: 10px;
+  }
+  .home-text07 {
+    font-size: 10px;
+  }
+  .home-text08 {
+    font-size: 10px;
+  }
+  .home-text09 {
+    font-size: 10px;
+  }
+  .home-text10 {
+    font-size: 10px;
+  }
+  .home-text11 {
+    font-size: 10px;
+  }
+  .home-text12 {
+    font-size: 10px;
+  }
+  .home-text13 {
+    font-size: 10px;
+  }
+  .home-text14 {
+    font-size: 10px;
+  }
+  .home-text15 {
+    font-size: 10px;
+  }
+  .home-text16 {
+    font-size: 10px;
+  }
+  .home-text17 {
+    font-size: 10px;
+  }
+  .home-text18 {
+    font-size: 10px;
+  }
+  .home-text19 {
+    font-size: 10px;
+  }
+  .home-text20 {
+    font-size: 10px;
+  }
+  .home-text21 {
+    font-size: 10px;
+  }
+  .home-text22 {
+    font-size: 10px;
+  }
+  .home-text23 {
+    font-size: 10px;
+  }
+  .home-text24 {
+    font-size: 10px;
+  }
+  .home-text25 {
+    font-size: 10px;
+  }
+  .home-text26 {
+    font-size: 10px;
+  }
+  .home-text27 {
+    font-size: 10px;
+  }
+  .home-text28 {
+    font-size: 12px;
+  }
+  .home-text29 {
+    font-size: 12px;
+  }
+  .home-text30 {
+    font-size: 12px;
+  }
+  .home-text31 {
+    font-size: 12px;
+  }
+  .home-text32 {
+    font-size: 12px;
+  }
+  .home-text33 {
+    font-size: 7px;
+  }
+  .home-text34 {
+    top: 149px;
+    left: 0px;
+    color: rgb(253, 2, 2);
+    right: 0px;
+    margin: auto;
+    font-size: 25px;
+    font-style: normal;
+    font-family: Merriweather;
+    font-weight: 400;
+  }
+}
+</style>
